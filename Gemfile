487e9ce2cde6c9fcd8a8271edbb9c3f98a6031ea
Alice Gray <alicegray33@gmail.com>
2022-01-11 15:51:24 +0000

    First user story complete

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source "https://rubygems.org"
+        gem 'sinatra'
+        gem 'webrick'
+        gem 'sinatra-contrib'
+        gem 'capybara'
+        gem 'selenium-webdriver'
+        gem 'rspec'
+        gem 'launchy'
---
+++ spec/features/testing_infrastructure_spec.rb
@@ -0,0 +1,16 @@
+feature 'Testing infrastructure working!' do
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Start new game'
+  end
+  scenario 'Allows players to enter their name' do
+    visit('/')
+    fill_in :player_1_name, with: 'John'
+    fill_in :player_2_name, with: 'Julie'
+    click_button 'Go!'
+
+    save_and_open_page
+    
+    expect(page).to have_content 'John vs Julie'
+  end
+end
