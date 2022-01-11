9befe98563ef2ede8c7d49000a3504de487da493
Alice Gray <alicegray33@gmail.com>
2022-01-11 16:40:41 +0000

    Attack player 2 and get confirmation

---
+++ spec/features/enter_names_spec.rb
@@ -0,0 +1,12 @@
+feature 'Enter names' do
+  scenario 'Can run app and check page content' do
+    visit('/')
+    expect(page).to have_content 'Start new game'
+  end
+  scenario 'Allows players to enter their name' do
+    sign_in_and_play
+    
+    expect(page).to have_content "John vs Julie"
+    
+  end
+end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,6 +2,13 @@ feature 'View hit points' do
 
   scenario 'see player 2 hitpoints' do
     sign_in_and_play
-    expect(page).to have_content "#{@name2}: 60HP"
+    save_and_open_page
+    expect(page).to have_content "Julie: 60HP"
+  end
+
+  scenario 'attack player 2 and get confirmation' do
+    sign_in_and_play
+    click_button 'Attack!'
+    expect(page).to have_content "You attacked Julie"
   end
 end
