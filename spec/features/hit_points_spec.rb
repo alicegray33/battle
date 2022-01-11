3dd6ae3ab37a258535143da6e0bf4ba3ae28a2a0
Alice Gray <alicegray33@gmail.com>
2022-01-11 16:56:16 +0000

    Using naughty global variables

--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -2,7 +2,7 @@ feature 'View hit points' do
 
   scenario 'see player 2 hitpoints' do
     sign_in_and_play
-    save_and_open_page
+    # save_and_open_page
     expect(page).to have_content "Julie: 60HP"
   end
 
